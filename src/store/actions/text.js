555cf5713e75079c8d58ca784f54dea688e90731
Richard Zilahi <richard.zilahi@salesframe.com>
2020-02-13 17:47:55 +0200

    flipping

--- src/store/actions/text.js
+++ src/store/actions/text.js
@@ -1,4 +1,4 @@
-import { SET_FONT_SIZE, SET_TEXT, SET_LETTER_SPACING, SET_LINE_HEIGHT } from './actionTypes'
+import { SET_FONT_SIZE, SET_TEXT, SET_LETTER_SPACING, SET_LINE_HEIGHT, TOGGLE_FLIPPED } from './actionTypes'
 
 export const setFontSize = fontSize => ({
 	type: SET_FONT_SIZE,
@@ -25,5 +25,12 @@ export const setLineHeight = lineHeight => ({
 	type: SET_LINE_HEIGHT,
 	payload: {
 		lineHeight,
+	},
+})
+
+export const toggleFlipped = isFlipped => ({
+	type: TOGGLE_FLIPPED,
+	payload: {
+		isFlipped,
 	},
 })
