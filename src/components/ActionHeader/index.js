9490a7c4149559381676545c44b60cc7335611e7
Richard Zilahi <richard.zilahi@salesframe.com>
2020-02-16 20:34:13 +0200

    actionHeader init

---
+++ src/components/ActionHeader/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './ActionHeader.module.scss'
+
+/**
+* @author zilahir
+* @function ActionHeader
+* */
+
+const ActionHeader = () => (
+	<div className={styles.actionHeaderContainer}>
+		ActionHeader
+	</div>
+)
+
+export default ActionHeader
