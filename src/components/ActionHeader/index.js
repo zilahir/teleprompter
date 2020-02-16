f756d49c3797b810e7feae53cb846fb52037ac19
Richard Zilahi <richard.zilahi@salesframe.com>
2020-02-16 22:05:20 +0200

    added loggedd in action header elements

--- src/components/ActionHeader/index.js
+++ src/components/ActionHeader/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import classnames from 'classnames'
 
-import { LINK, LOGIN, REGISTER } from '../../utils/consts'
+import { LINK, LOGIN, REGISTER, SAVE, SAVE_AS_COPY, LOAD } from '../../utils/consts'
 import Button from '../common/Button'
 import styles from './ActionHeader.module.scss'
 import Login from '../Login'
@@ -21,24 +22,74 @@ const ActionHeader = () => {
 		toggleLogin(false)
 		toggleRegister(!showRegister)
 	}
+	const isLoggedIn = true
 	return (
-		<div className={styles.actionHeaderContainer}>
-			<ul className={styles.actionList}>
-				<li>
-					<Button
-						labelText="Sign Up"
-						onClick={() => openRegisterBox()}
-						type={LINK}
-					/>
-				</li>
-				<li>
-					<Button
-						labelText="Login"
-						onClick={() => openLoginBox()}
-						type={LINK}
-					/>
-				</li>
-			</ul>
+		<div className={classnames(
+			styles.actionHeaderContainer,
+			!isLoggedIn ? styles.alignEnd : null,
+		)}
+		>
+			{
+				isLoggedIn
+					? (
+						<ul className={styles.loggedInActionList}>
+							<li>
+								<Button
+									labelText="Save"
+									onClick={() => null}
+									type={LINK}
+								/>
+							</li>
+							<li>
+								<Button
+									labelText="Save As Copy"
+									onClick={() => null}
+									type={LINK}
+								/>
+							</li>
+							<li>
+								<Button
+									labelText="Load"
+									onClick={() => null}
+									type={LINK}
+								/>
+							</li>
+						</ul>
+					)
+					: null
+			}
+			{
+				!isLoggedIn
+					? (
+						<ul className={styles.actionList}>
+							<li>
+								<Button
+									labelText="Sign Up"
+									onClick={() => openRegisterBox()}
+									type={LINK}
+								/>
+							</li>
+							<li>
+								<Button
+									labelText="Login"
+									onClick={() => openLoginBox()}
+									type={LINK}
+								/>
+							</li>
+						</ul>
+					)
+					: (
+						<ul className={styles.actionList}>
+							<li>
+								<Button
+									labelText="Log Out"
+									onClick={() => openRegisterBox()}
+									type={LINK}
+								/>
+							</li>
+						</ul>
+					)
+			}
 			<Login
 				isVisible={showLogin}
 				type={LOGIN}
