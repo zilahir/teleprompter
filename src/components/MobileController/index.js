135297f14159b7708309cc8920c3c6315d184484
Richard Zilahi <richard.zilahi@salesframe.com>
2020-03-18 21:21:14 +0200

    implementing buttons

--- src/components/MobileController/index.js
+++ src/components/MobileController/index.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 import playPause from '../../assets/controls/play.svg'
+import backward from '../../assets/controls/backward.svg'
+import forward from '../../assets/controls/forward.svg'
 import styles from './MobileContainer.module.scss'
 
 /**
@@ -17,17 +19,33 @@ const MobileController = ({ prompterId }) => {
 	}
 	return (
 		<div className={styles.mainContainer}>
-			<div
-				className={classnames(
+			<div className={styles.middle}>
+				<div className={classnames(
 					styles.oneButton,
-					styles.playPause,
+					styles.dirButton,
 				)}
-				role="button"
-				onClick={() => handleStartStop()}
-				onKeyDown={null}
-				tabIndex={-1}
-			>
-				<img alt="play" src={playPause} />
+				>
+					<img src={backward} alt="backwards" />
+				</div>
+				<div
+					className={classnames(
+						styles.oneButton,
+						styles.playPause,
+					)}
+					role="button"
+					onClick={() => handleStartStop()}
+					onKeyDown={null}
+					tabIndex={-1}
+				>
+					<img alt="play" src={playPause} />
+				</div>
+				<div className={classnames(
+					styles.oneButton,
+					styles.dirButton,
+				)}
+				>
+					<img src={forward} alt="backwards" />
+				</div>
 			</div>
 		</div>
 	)
