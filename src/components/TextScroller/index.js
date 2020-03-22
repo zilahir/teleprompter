b50731689cffaf7987da87e8ae677ed990851640
Richard Zilahi <richard.zilahi@salesframe.com>
2020-03-22 02:23:26 +0200

    implementing playing with socker

--- src/components/TextScroller/index.js
+++ src/components/TextScroller/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import KeyboardEventHandler from 'react-keyboard-event-handler'
+import useSocket from 'use-socket.io-client'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { motion, useAnimation } from 'framer-motion'
@@ -23,11 +24,12 @@ const Scroller = styled.div`
 `
 
 const TextScroller = props => {
-	const { text, scrollSpeed, prompterObject, playing } = props
+	const [socket] = useSocket(process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.NODE_ENV === 'production')
+	const { text, scrollSpeed, prompterObject } = props
 	const controls = useAnimation()
 	const textRef = useRef(null)
 	const [height, setHeight] = useState(null)
-	const [isPlaying, togglePlaying] = useState(playing)
+	const [playing, togglePlaying] = useState(false)
 	const container = {
 		start: {
 			y: 0,
@@ -40,13 +42,18 @@ const TextScroller = props => {
 	useEffect(() => {
 		const { clientHeight } = textRef.current
 		setHeight(clientHeight)
-	}, [text, isPlaying])
+	}, [text])
+
+	socket.on('isPlaying', ({ prompterId, isPlaying }) => {
+		togglePlaying(isPlaying)
+		console.debug(`prompterId: ${prompterId}, isPlaying: ${isPlaying}`)
+	})
 
 	function handleKeyPress(key, e) {
 		e.preventDefault()
 		if (key === SPACE) {
-			togglePlaying(!isPlaying)
-			if (isPlaying) {
+			togglePlaying(!playing)
+			if (playing) {
 				controls.stop()
 			} else {
 				controls.start('end')
@@ -86,7 +93,6 @@ const TextScroller = props => {
 }
 
 TextScroller.propTypes = {
-	playing: PropTypes.bool.isRequired,
 	prompterObject: PropTypes.objectOf(PropTypes.any).isRequired,
 	scrollSpeed: PropTypes.number.isRequired,
 	text: PropTypes.string.isRequired,
