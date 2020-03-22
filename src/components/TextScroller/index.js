d7e5fbf4abeacfe9f8566074f3d3013bffdc61e8
Richard Zilahi <richard.zilahi@salesframe.com>
2020-03-23 01:46:25 +0200

    reimplementing scroll

--- src/components/TextScroller/index.js
+++ src/components/TextScroller/index.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import KeyboardEventHandler from 'react-keyboard-event-handler'
 import useSocket from 'use-socket.io-client'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { motion, useAnimation } from 'framer-motion'
 
 import styles from './TextScroller.module.scss'
 import { keyListeners, SPACE, F6 } from '../../utils/consts'
@@ -26,31 +25,15 @@ const Scroller = styled.div`
 const TextScroller = props => {
 	const [socket] = useSocket(process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.NODE_ENV === 'production')
 	const { text, scrollSpeed, prompterObject } = props
-	const controls = useAnimation()
 	const textRef = useRef(null)
-	const [height, setHeight] = useState(null)
 	const [playing, togglePlaying] = useState(false)
-	const container = {
-		start: {
-			y: 0,
-		},
-		end: {
-			y: -height - 100,
-		},
-	}
-
-	useEffect(() => {
-		const { clientHeight } = textRef.current
-		setHeight(clientHeight)
-	}, [text])
-
 	socket.on('isPlaying', ({ prompterId, isPlaying }) => {
 		togglePlaying(isPlaying)
 		console.debug(`prompterId: ${prompterId}, isPlaying: ${isPlaying}`)
 		if (isPlaying) {
-			controls.start('end')
+			// STOP
 		} else {
-			controls.stop()
+			// START
 		}
 	})
 
@@ -59,9 +42,9 @@ const TextScroller = props => {
 		if (key === SPACE) {
 			togglePlaying(!playing)
 			if (playing) {
-				controls.stop()
+				// START
 			} else {
-				controls.start('end')
+				// STOP
 			}
 		} else if (key === F6) {
 			toggleFullScreen()
@@ -76,10 +59,7 @@ const TextScroller = props => {
 				letterSpacing={prompterObject.letterSpacing}
 				scrollWidth={prompterObject.scrollWidth}
 			>
-				<motion.div
-					animate={controls}
-					variants={container}
-					transition={{ ease: 'linear', duration: (scrollSpeed * scrollSpeed) * 0.5 }}
+				<div
 					className={styles.scroller}
 				>
 					<p
@@ -87,7 +67,7 @@ const TextScroller = props => {
 					>
 						{text}
 					</p>
-				</motion.div>
+				</div>
 			</Scroller>
 			<KeyboardEventHandler
 				handleKeys={[...keyListeners]}
