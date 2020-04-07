4931f19e9aff168a45b62d6e9cbf6a874ca18ace
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-07 22:13:01 +0300

    getting rid of warning

--- src/components/TextScroller/index.js
+++ src/components/TextScroller/index.js
@@ -156,7 +156,7 @@ const TextScroller = props => {
 		<div className={styles.rootContainer}>
 			<Scroller
 				className={styles.scrollerContainer}
-				fontSize={prompterObject.fontSize * 30}
+				fontSize={String(prompterObject.fontSize * 30)}
 				lineHeight={prompterObject.lineHeight}
 				letterSpacing={prompterObject.letterSpacing}
 				scrollWidth={prompterObject.scrollWidth}
