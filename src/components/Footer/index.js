3c51027489d7abf25f36d37f6cde61a2af4665a9
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-10 11:38:22 +0300

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,49 @@
 import React from 'react'
 import { Row, Container, Col } from 'react-grid-system'
+import styled from 'styled-components'
+import Icon from 'react-icons-kit'
+import { github } from 'react-icons-kit/feather/github'
 
 import styles from './Footer.module.scss'
+import { Colors } from '../../utils/consts'
 
 /**
 * @author zilahir
 * @function Footer
 * */
 
+const IconContainer = styled.div`
+	color: ${props => props.color};
+`
+
 const Footer = () => (
 	<footer className={styles.footerContainer}>
 		<Container fluid>
-			<Row>
-				<Col lg={12}>
+			<Row
+				align="center"
+			>
+				<Col lg={6}>
+					<div className={styles.innerContainer}>
+						<p>
+							prompter.me
+						</p>
+						<p className={styles.purple}>
+							<a
+								rel="noopener noreferrer"
+								target="_blank"
+								href="https://www.netlify.com"
+							>
+								This site is powered by Netlify
+							</a>
+						</p>
+					</div>
+				</Col>
+				<Col lg={6}>
 					<ul>
 						<li>
-							<p>
-								lofasz
-							</p>
+							<IconContainer color={Colors.purple}>
+								<Icon size="1.5em" icon={github} />
+							</IconContainer>
 						</li>
 					</ul>
 				</Col>
