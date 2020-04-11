0a93868a62751ea84eb9b4970e85d4db279012ca
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-11 15:16:55 +0300

    implementing password recovery

--- src/components/Password/index.js
+++ src/components/Password/index.js
@@ -41,13 +41,13 @@ const Password = () => {
 		console.debug('render', slug, token)
 		const passwordRecoveryRequest = getPasswordResetObject(slug)
 		passwordRecoveryRequest.then(res => {
-			if (res.isUsed || res.expiresAt > new Date().getMinutes()) {
+			if (res.isUsed || res.expiresAt < new Date().getMinutes()) {
 				setAlertMessage({
 					text: 'This password reset had expired',
 					state: 'error',
 				})
 			} else {
-				setUserId(res.userId)
+				// setUserId(res.userId)
 				toggleHidden(false)
 			}
 		})
