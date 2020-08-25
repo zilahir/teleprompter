1e046bb79d69ba98f875734c90fccafa5e56eeda
Richard Zilahi <richard.zilahi@salesframe.com>
2020-08-25 19:48:46 +0300

    implementing segments

--- src/components/Segment/index.js
+++ src/components/Segment/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Icon from 'react-icons-kit'
@@ -20,43 +20,58 @@ const SegmentIndicator = styled.span`
 	background-color: ${props => props.segmentColor};
 `
 
+const SegmentText = styled.textarea`
+	height: ${props => props.height}px;
+`
+
 const Segment = ({
 	segmentTitle,
 	segmentColor,
 	segmentText,
-}) => (
-	<OnseSegment
-		className={styles.oneSegment}
-		borderColor={segmentColor}
-	>
-		<div className={styles.segmentHeader}>
-			<Input
-				labelText={null}
-				inputClassName={styles.segmentName}
-				inheritedValue={segmentTitle}
-			/>
-			<ul>
-				<li>
-					<SegmentIndicator
-						segmentColor={segmentColor}
-						className={styles.segmentColorIndicator}
-					/>
-				</li>
-				<li>
-					<button type="button" className={styles.deleteBtn}>
-						<Icon icon={times} />
-					</button>
-				</li>
-			</ul>
-		</div>
-		<div className={styles.segmentBody}>
-			<p>
-				{segmentText}
-			</p>
-		</div>
-	</OnseSegment>
-)
+}) => {
+	const thisSegmentRef = useRef(null)
+	const [scrollHeight, setScrollHeight] = useState()
 
+	useEffect(() => {
+		if (thisSegmentRef.current) {
+			setScrollHeight(thisSegmentRef.current.scrollHeight)
+		}
+	}, [])
+	return (
+		<OnseSegment
+			className={styles.oneSegment}
+			borderColor={segmentColor}
+		>
+			<div className={styles.segmentHeader}>
+				<Input
+					labelText={null}
+					inputClassName={styles.segmentName}
+					inheritedValue={segmentTitle}
+				/>
+				<ul>
+					<li>
+						<SegmentIndicator
+							segmentColor={segmentColor}
+							className={styles.segmentColorIndicator}
+						/>
+					</li>
+					<li>
+						<button type="button" className={styles.deleteBtn}>
+							<Icon icon={times} />
+						</button>
+					</li>
+				</ul>
+			</div>
+			<div className={styles.segmentBody}>
+				<SegmentText
+					ref={thisSegmentRef}
+					value={segmentText}
+					height={scrollHeight}
+				/>
+			</div>
+		</OnseSegment>
+	)
+}
 
 Segment.propTypes = {
 	segmentColor: PropTypes.string.isRequired,
