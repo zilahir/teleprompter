4aa2acdffc111e09d436314706df9c48b035ad22
Richard Zilahi <richard.zilahi@salesframe.com>
2020-09-07 21:12:37 +0300

    adjusting scrolling behaviour

--- src/components/TextScroller/index.js
+++ src/components/TextScroller/index.js
@@ -63,7 +63,7 @@ const TextScroller = props => {
 	const bottomRef = useRef(null)
 	const [playing, setPlaying] = useState(false)
 	const [position, setPosition] = useState(0)
-	const [STEP, setStep] = useState(5)
+	const [STEP, setStep] = useState(1)
 	const [scrollSpeedValue, setScrollSpeedValue] = useState(scrollSpeed)
 	const scrollerRef = useRef(null)
 	const { slug } = useParams()
@@ -73,6 +73,7 @@ const TextScroller = props => {
 		setPosition(position + STEP)
 		scrollerRef.current.scroll({
 			top: position,
+			behavior: 'smooth',
 		})
 	}, playing ? scrollSpeedValue : null)
 
@@ -107,7 +108,10 @@ const TextScroller = props => {
 	}
 
 	useEffect(() => {
-		scrollerRef.current.scroll({ top: position })
+		scrollerRef.current.scroll({
+			top: position,
+			behavior: 'smooth',
+		})
 	}, [position])
 
 	useEffect(() => {
