d299d3ae29a6d5347b2e7b75d09fa834ecf196e0
Richard Zilahi <richard.zilahi@salesframe.com>
2021-03-27 18:18:05 +0200

    style(eslint): removed unused eslint rule disabling comments

--- src/components/Segment/index.js
+++ src/components/Segment/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useContext, useRef, useState } from 'react'
